8a6c731f82b44a368bae0fc0380cb435b4ce27a9
Tomasz Eichler <tomasz.eichler@wp.pl>
2020-06-10 23:09:25 +0200

    Added simple version of anagram function

---
+++ anagram.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <algorithm>
+#include <iostream>
+
+bool checkIfAnagram(std::string first, std::string second) {
+    std::sort(first.begin(), first.end());
+    std::sort(second.begin(), second.end());
+
+    return first == second;
+}
+
+int main() 
+{ 
+    std::string str1 = "TOmek Ala"; 
+    std::string str2 = "lTa AOkem"; 
+    if (checkIfAnagram(str1, str2)) {
+        std::cout << "ANAGRAM\n"; 
+    } else {
+        std::cout << "NOT ANAGRAM\n"; 
+    }
+  
+    return 0; 
+} 
