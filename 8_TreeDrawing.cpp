64a2a06b18c67df4af8fb34ed7bfa67b51844a4c
Kali Live user <wiktorbigaj@wp.pl>
2020-06-11 15:07:22 +0000

    Fixed some code formatting

--- 8_TreeDrawing.cpp
+++ 8_TreeDrawing.cpp
@@ -8,7 +8,7 @@ void printTree(int height){
     } else {
         std::vector<std::string> tree(height);
         std::generate(tree.begin(), tree.end(), 
-            [spaceCount = height - 1,starCount=1] () mutable {
+            [spaceCount = height - 1,starCount = 1] () mutable {
                 std::string part1(spaceCount, ' ');
                 std::string part2(starCount, '*');
                 spaceCount--;
