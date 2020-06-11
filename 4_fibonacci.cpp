9dbae77c8138819231f20d064ded29c32c15f899
Marcinati <marcin.kapusta.pk@gmail.com>
2020-06-11 15:03:28 +0200

    +1 from Szafirmag

--- 4_fibonacci.cpp
+++ 4_fibonacci.cpp
@@ -10,11 +10,35 @@ std::array<int, howManyElements> fibo {1};
 
 void printFibo (std::array<int, howManyElements> a) {
    std::copy(begin(a), end(a), std::ostream_iterator<int> {std::cout, " "});
+   std::cout << '\n';
+}
+
+int getFibFromVector(int fibnum){
+  if(fibnum < 0){
+    return -1;
+  }
+  if(fibnum == 0){
+    return 0;
+  }
+  std::vector<int> fibVec(fibnum);
+  std::generate(fibVec.begin(), fibVec.end(), 
+            [first = 0, second = 1] () mutable {
+              int tmp = second;
+              second += first;
+              first = tmp;
+              return first;
+            });
+    std::all_of(fibVec.begin(), fibVec.end(), [](auto& el){std::cout<<el<<' '; return true;});
+    return fibVec.at(fibnum -1);
 }
 
 int main() {
+    //1
     std::adjacent_difference(begin(fibo), std::prev(end(fibo)), std::next(begin(fibo)), std::plus<> {});
     printFibo(fibo);
+
+    //2
+    getFibFromVector(howManyElements);
 
     return 0;
 }
