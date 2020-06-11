0a5dcb148c447ff39b4b6e51ef190d29e8304ec1
Karol Dulemba <kdulemba@gmail.com>
2020-06-11 16:22:36 +0200

    removeWords - the worst idea ;)

--- 6_removeWords.cpp
+++ 6_removeWords.cpp
@@ -5,6 +5,12 @@
 #include <iterator>
 #include <utility>
 
+
+
+
+// Loops are only used to cout actual variables status, and see what's going on in my program :). 
+
+
 int main() {
 
   std::string sen("Ala ma kota a kot ma Ale, jednak to bylo dawno temu.");
@@ -40,26 +46,27 @@ mutable{
   });
 
 for (auto el : words_to_remove) std::cout <<"First letter to remove: "<<el.first << " and how many letter to remove: " << el.second << '\n';
-int iterator2 = 0;
 
-std::count_if(words_to_remove.begin(), words_to_remove.end(), [&sen, words_to_remove, &iterator2](auto pair)
-mutable{  
-     //std::string::iterator it=sen.begin();
-     
 
-     std::remove_if(sen.begin(), sen.end(),[words_to_remove, pair, &iterator2 ](auto letter){
-     if(iterator2>=pair.first && iterator2<=(pair.first+pair.second-1))
-     {  std::cout<<"jestem w warunku"<<'\n';
-        return true;
-        
-     }
-     iterator2++;
-     std:: cout<< iterator2 << '\n'; 
-     //it++; 
+std::count_if(words_to_remove.begin(), words_to_remove.end(), [&sen, words_to_remove](auto pair)
+mutable{ int iterator = 0; 
        
-     return false; });
-
+     std::replace_if(sen.begin(), sen.end(),[words_to_remove, pair, &iterator ](auto letter){
+     
+     if(iterator>=pair.first && iterator<=(pair.first+pair.second-1))
+     {  
+        std::cout<<"jestem w warunku"<<'\n';
+        iterator++;
+        return true;
+     }
+     iterator++;
+     return false; }, '#');
  return 0; 
  });
- std::cout << sen;
+
+std::cout << sen;
+sen.erase(std::remove(sen.begin(), sen.end(), '#'), sen.end());
+
+sen.shrink_to_fit();
+std::cout << '\n' << sen;
 }
