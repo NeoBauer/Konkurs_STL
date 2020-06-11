ab052cb269f12d768a0a9ccfd2dffa03eda2e0a7
Tomasz Eichler <tomasz.eichler@wp.pl>
2020-06-11 11:36:00 +0200

    Added PrepareString function to remove spaces and punctuation marks

--- anagram.cpp
+++ anagram.cpp
@@ -2,7 +2,19 @@
 #include <iostream>
 #include <string>
 
+void PrepareString(std::string& text) {
+    auto deleteSpecialChars = [](char& c) {
+        c = std::tolower(c);
+        return std::isspace(c) || std::ispunct(c);
+    };
+
+    text.erase(std::remove_if(text.begin(), text.end(), deleteSpecialChars), text.end());
+}
+
 bool checkIfAnagram(std::string first, std::string second) {
+    PrepareString(first);
+    PrepareString(second);
+
     std::sort(first.begin(), first.end());
     std::sort(second.begin(), second.end());
 
@@ -10,8 +22,8 @@ bool checkIfAnagram(std::string first, std::string second) {
 }
 
 int main() {
-    std::string str1 = "TOmek Ala";
-    std::string str2 = "lTa AOkem";
+    std::string str1 = "Tomek";
+    std::string str2 = "TomekG";
     if (checkIfAnagram(str1, str2)) {
         std::cout << "ANAGRAM\n";
     } else {
